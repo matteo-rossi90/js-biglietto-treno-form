d60ea9a44cb74d83ec7f997d8fd4e21822674f7f
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-05 13:48:08 +0200

    Impostazione iniziale - creazione file html e css di base + definizione degli step in js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere - Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole: - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65 */
+
+// fare in modo che il nome, i chilometri da percorrere e 
+//l'età del passeggero nel form vengano salvati
+
+//calcolare il prezzo in base al numero di chilometri
+
+//applicare sconti in base all'età
+
+//generare un output
